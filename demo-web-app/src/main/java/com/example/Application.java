f696513633208ec1d782b436d4ad5a20b5703627
Supershuu <2426230872@qq.com>
2023-10-18 14:17:46 +0800

    :sparkles: 初始化spring-boot项目

--- demo-web-app/src/main/java/com/example/Application.java
+++ demo-web-app/src/main/java/com/example/Application.java
@@ -1,4 +1,26 @@
 package com.example;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        // 关闭banner(打印信息)
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run(args);
+        log.info(">>> {}",Application.class.getSimpleName().toUpperCase() + " STARTING SUCCESS");
+    }
+    @GetMapping("/")
+    public String index() {
+        return "WELCOME";
+    }
 }
